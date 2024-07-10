fc3e482834aecde394d4f12603462b768a186223
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-10 18:22:21 +0300

    Enhance Java path resolver to include second-level directories
    Extended the `GetInstalledJavaVersions` method to include directories from the second level, ensuring all relevant versions are considered. Improved efficiency and maintainability by consolidating directory listings and avoiding duplicate entries.

--- src/Java/MinecraftJavaPathResolver.cs
+++ src/Java/MinecraftJavaPathResolver.cs
@@ -20,31 +20,41 @@ public class MinecraftJavaPathResolver : IJavaPathResolver
         if (!dir.Exists)
             return [];
 
-        return dir.GetDirectories()
+        var topLevelDirectories = dir.GetDirectories();
+
+        var secondLevelDirectories = topLevelDirectories
+            .SelectMany(d => d.GetDirectories())
+            .ToArray();
+
+        var allDirectories = topLevelDirectories.Concat(secondLevelDirectories);
+
+        return allDirectories
             .Select(x => x.Name)
+            .Distinct()
             .ToArray();
     }
+
 
     public string? GetDefaultJavaBinaryPath(RulesEvaluatorContext rules)
     {
         var javaVersions = GetInstalledJavaVersions();
         string? javaPath = null;
-        
+
         if (string.IsNullOrEmpty(javaPath) &&
             javaVersions.Contains(MinecraftJavaPathResolver.JreLegacyVersion.Component))
             javaPath = GetJavaBinaryPath(MinecraftJavaPathResolver.JreLegacyVersion, rules);
-        
+
         if (string.IsNullOrEmpty(javaPath) &&
             javaVersions.Contains(MinecraftJavaPathResolver.CmlLegacyVersion.Component))
             javaPath = GetJavaBinaryPath(MinecraftJavaPathResolver.CmlLegacyVersion, rules);
 
-        if (string.IsNullOrEmpty(javaPath) && 
+        if (string.IsNullOrEmpty(javaPath) &&
             javaVersions.Length > 0)
             javaPath = GetJavaBinaryPath(new JavaVersion(javaVersions[0]), rules);
 
         return javaPath;
     }
-    
+
     public string GetJavaBinaryPath(JavaVersion javaVersionName, RulesEvaluatorContext rules)
     {
         var runtime = GetJavaDirPath(javaVersionName, rules);
@@ -61,9 +71,9 @@ public class MinecraftJavaPathResolver : IJavaPathResolver
         };
     }
 
-    public string GetJavaDirPath(JavaVersion javaVersionName, RulesEvaluatorContext rules) 
+    public string GetJavaDirPath(JavaVersion javaVersionName, RulesEvaluatorContext rules)
         => Path.Combine(
-            _path.Runtime, 
-            MinecraftJavaManifestResolver.GetOSNameForJava(rules.OS), 
+            _path.Runtime,
+            MinecraftJavaManifestResolver.GetOSNameForJava(rules.OS),
             javaVersionName.Component);
 }
